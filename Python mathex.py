4acacfe337f27af115bb21f6a7db450ddd14523f
tartignaule <samuel25fortier@gmail.com>
2022-12-07 19:18:44 +0100

    exercice 3 fini

---
+++ Python mathex.py
@@ -0,0 +1,24 @@
+﻿import math
+
+def div(n):
+    l=[]
+    for d in range (1,n+1):
+        if n%d==0:
+            l.append(d)
+    return l
+
+def div2():
+    l2=[]
+    for i in range (1,10000):
+        if len(div(i)) == 13:
+            l2.append(i)
+    return l2
+
+def parf():
+    l3=[]
+    for i in range (1,10000):
+        x=div(i)
+        x.pop()
+        if sum(x) == i:
+            l3.append(i)
+    return l3
